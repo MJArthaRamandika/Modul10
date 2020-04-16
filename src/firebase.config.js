49cbdd32d43b85bfd6bdcf8e52712016bcfc7133
MJArthaRamandika <ramandikajovanka@gmail.com>
2020-04-16 15:03:42 +0700

    Menambah Firebase dan menambah isi file

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyD0sl3WjAIMmFS-3B7xaqpqldnAoR7Pdpo",
+    authDomain: "fir-reactjs-eba09.firebaseapp.com",
+    databaseURL: "https://fir-reactjs-eba09.firebaseio.com",
+    projectId: "fir-reactjs-eba09",
+    storageBucket: "fir-reactjs-eba09.appspot.com",
+    messagingSenderId: "117980558462",
+    appId: "1:117980558462:web:a9f897b5e756501505d42f",
+    measurementId: "G-9QHF4J2DFB"
+}
